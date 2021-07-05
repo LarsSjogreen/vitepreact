1a4cc6605e280218dc641a9b99f1616876ad9318
larssjogreen <larsfredrikanderssonsjogreen@gmail.com>
2021-07-05 15:41:43 +0200

    Lite mer exempelkod

--- src/Compo/compo.jsx
+++ src/Compo/compo.jsx
@@ -1,9 +1,11 @@
 import { Compadre } from "./compadre";
+import Clock from "./Clock";
 
 export const Compo = () => (
     <div>
         <p>Snappy och snabbt. Verkar ladda om fint också.</p>
         <p>Väldigt snabbt och fint.</p>
         <Compadre/>
+        <Clock/>
     </div>
 );
